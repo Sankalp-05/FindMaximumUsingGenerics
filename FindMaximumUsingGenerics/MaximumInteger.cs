da38dfa54f4e80fcdd594585f64f9a04b126461b
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-21 10:29:03 +0530

    [Sankalp]- Find Maximum Integer Number

---
+++ FindMaximumUsingGenerics/MaximumInteger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class MaximumInteger
+    {
+        public static int MaxIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            //Condition to Compare the firstValue to secondValue and thirdValue
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            //Condition to Compare the secondValue to firstValue and thirdValue
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 || secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 || secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            //Condition to Compare the thirdValue to secondValue and firstValue
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 || thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 || thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            //return value if all the numbers are equal
+            return firstValue;
+        }
+    }
+}
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace FindMaximumUsingGenerics
+﻿using System;
+
+namespace FindMaximumUsingGenerics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum Number Using Generics Program ");
+            Console.WriteLine("1.Compare three integer numbers");
+            Console.WriteLine("Please choose the option to execute : ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            while (option < 1 || option > 7)
+            {
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            switch (option)
+            {
+
+                case 1:
+                    Console.WriteLine("Executing three test cases for Finding Maximum Integer Value");
+                    Console.WriteLine(MaximumInteger.MaxIntegerNumber(20, 20, 20));
+                    Console.WriteLine(MaximumInteger.MaxIntegerNumber(65, 20, 10));
+                    Console.WriteLine(MaximumInteger.MaxIntegerNumber(5, 45, 98));
+                    break;
+            }
         }
     }
 }
